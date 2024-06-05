0dacf383dddc5bfa91851cde7a145712a90efa99
seungkyunIm <hiisk123@naver.com>
2024-06-05 12:31:58 +0900

    추가 언어팩

--- src/ResultPage.java
+++ src/ResultPage.java
@@ -2,6 +2,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,8 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Result")
 public class ResultPage extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setCharacterEncoding("UTf-8");
-		response.setContentType("text/html; charset=UTF-8");
+		Util.ko(request, response);
 		System.out.println("럭키");
 		int cnt = Integer.parseInt(request.getParameter("cnt"));
 		String txt = request.getParameter("txt");		
@@ -28,7 +28,8 @@ public class ResultPage extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		request.setCharacterEncoding("UTF-8");                                // body에 직접 받아오기 때문에 response 가 아닌 request에 먼저 한글 자료를 담는다.
 //		response.setCharacterEncoding("UTf-8");
-//		response.setContentType("text/html; charset=UTF-8");
+//		response.setContentType("text/html; charset=UTF-8");                  // 아래 메소드 공통 사용으로 위에 3줄은 따로 안써줘도 된다.
+//		Util.ko(request, response);
 //		System.out.println("락키");
 //		int cnt = Integer.parseInt(request.getParameter("cnt"));
 //		String txt = request.getParameter("txt");		
@@ -39,4 +40,5 @@ public class ResultPage extends HttpServlet {
 //			pw.append((i+1)+ " "+txt + "</br>");
 //		}
 	}
+	
 }
